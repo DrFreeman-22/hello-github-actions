d84c3fae0b529fa178b33458776f59387dbd489d
cs-iliev <93545848+cs-iliev@users.noreply.github.com>
2021-11-11 13:47:28 +0200

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "echo The person who forkerd this repo's default branch is called $INPUT_MY_NAME"
+sh -c "echo The person who forkerd this repo\'s default branch is called $INPUT_MY_NAME"
